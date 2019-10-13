8cfd3cb5db16d5710f51ec3b52f89543bdc644aa
kishor4kumar@yahoo.com <kishor4kumar@yahoo.com>
2019-10-13 09:11:08 +0530

    Events example

---
+++ Basics/Events.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Basics 
+{  
+    namespace Events 
+    {
+        public struct Game
+        {
+            public string Name;
+            public string Developer;
+            public string Platform;
+
+            public Game(string name, string developer, string platform)
+            {
+                Name = name;
+                Developer = developer;
+                Platform = platform;
+            }
+        }
+
+        public class GameEventArgs : EventArgs
+        {
+            public Game Game;
+            public GameEventArgs(Game game)
+            {
+                Game = game;
+            }
+        }
+
+        public class GameLibrary
+        {
+            private readonly IList<Game> games = new List<Game>();
+
+            public delegate void GameAddedHandler(object sender, GameEventArgs eventArgs);
+            public event GameAddedHandler GameAdded;
+
+            public void AddGame(Game game)
+            {
+                games.Add(game);
+                GameAdded?.Invoke(this, new GameEventArgs(game));
+            }
+        }
+
+        public class GamerCustomer
+        {
+            private readonly GameLibrary library;
+            
+            public IList<Game> InterestedGames
+            {
+                get; private set;
+            }
+
+            private GamerCustomer() { }
+
+            public GamerCustomer(GameLibrary gameLibrary)
+            {
+                InterestedGames = new List<Game>();
+                library = gameLibrary;
+                library.GameAdded += GameAddedInLibrary;
+            }
+
+            private void GameAddedInLibrary(object sender, GameEventArgs eventArgs)
+            {
+                if(eventArgs.Game.Name.Contains("God of War") || eventArgs.Game.Developer == "Ubisoft")
+                {
+                    InterestedGames.Add(eventArgs.Game);
+                }
+            }
+        }
+
+    }
+}
---
+++ BasicsTest/EventsTest.cs
@@ -0,0 +1,33 @@
+﻿using NUnit.Framework;
+using Basics.Events;
+
+namespace BasicsTest
+{
+    class EventsTest
+    {
+        [Test]
+        public void BasicEventsTest()
+        {
+            GameLibrary gameLibrary = new GameLibrary();
+            GamerCustomer gamerCustomer = new GamerCustomer(gameLibrary);
+
+            gameLibrary.AddGame(new Game("Uncharted", "Naughty Dog", "PS4"));
+            Assert.AreEqual(0, gamerCustomer.InterestedGames.Count);
+
+            gameLibrary.AddGame(new Game("God of War 3", "Santa Monica", "PS4"));
+            Assert.AreEqual(1, gamerCustomer.InterestedGames.Count);
+
+            gameLibrary.AddGame(new Game("Last of US", "Naughty Dog", "PS4"));
+            Assert.AreEqual(1, gamerCustomer.InterestedGames.Count);
+
+            gameLibrary.AddGame(new Game("God of War 2018", "Santa Monica", "PS4"));
+            Assert.AreEqual(2, gamerCustomer.InterestedGames.Count);
+
+            gameLibrary.AddGame(new Game("Last of US", "Naughty Dog", "PS4"));
+            Assert.AreEqual(2, gamerCustomer.InterestedGames.Count);
+
+            gameLibrary.AddGame(new Game("Odessy", "Ubisoft", "PS4"));
+            Assert.AreEqual(3, gamerCustomer.InterestedGames.Count);
+        }
+    }
+}
