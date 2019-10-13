c62a09e58a5003d5ca8630c7e3d04a77fe68eaa7
kishor4kumar@yahoo.com <kishor4kumar@yahoo.com>
2019-10-13 10:44:06 +0530

    Threading

--- Advanced/BasicThread.cs
+++ Advanced/BasicThread.cs
@@ -1,6 +1,44 @@
-﻿namespace Advanced
+﻿using System;
+using System.Threading;
+
+namespace Advanced
 {
     public class BasicThread
     {
+        private static readonly AutoResetEvent autoResetEvent1 = new AutoResetEvent(false);
+        private static readonly AutoResetEvent autoResetEvent2 = new AutoResetEvent(false);
+
+        public static void RunThreads()
+        {
+            Thread thread1 = new Thread(WriteToConsole);
+            Thread thread2 = new Thread(WriteLines);
+
+            thread1.Start();
+            thread2.Start();
+
+            thread1.Join();
+            thread2.Join();
+
+        }
+
+        private static void WriteToConsole()
+        {
+            for(int i=0; i<20; i++)
+            {
+                autoResetEvent1.Set();
+                Console.WriteLine("Func 1 - " + i);
+                autoResetEvent2.WaitOne();
+            }
+        }
+
+        private static void WriteLines()
+        {
+            for (int i = 0; i < 20; i++)
+            {
+                autoResetEvent1.WaitOne();
+                Console.WriteLine("Func 2 - " + i);
+                autoResetEvent2.Set();
+            }
+        }
     }
 }
---
+++ Advanced/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Advanced
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            BasicThread.RunThreads();
+            Console.ReadLine();
+        }
+    }
+}
