e307394b7fc0b7714cc6adf2186e38e03733a103
kishor4kumar@yahoo.com <kishor4kumar@yahoo.com>
2019-10-30 19:17:39 +0530

    Adpter pattern

--- DesignPatterns/Structural/Adapter.cs
+++ DesignPatterns/Structural/Adapter.cs
@@ -1,6 +1,74 @@
-﻿namespace DesignPatterns.Structural
+﻿using System;
+using System.Collections.Generic;
+
+namespace DesignPatterns.Structural
 {
-    class Adapter
+
+    class MemeLord
     {
+        public MemeLord()
+        {
+            var persons = new List<IPerson>
+            {
+                new Millennials(),
+                new BoomersAdapter(new Boomers()),
+                new Millennials(),
+                new BoomersAdapter(new Boomers())
+            };
+
+            Console.WriteLine("Dark meme reaction:");
+            
+            //darker meme
+            foreach(var person in persons)
+            {
+                Console.WriteLine(person.GetMemeReaction(true));
+            }
+
+            Console.WriteLine("Normal meme reaction:");
+            //normal meme
+            foreach (var person in persons)
+            {
+                Console.WriteLine(person.GetMemeReaction(false));
+            }
+
+        }
+    }
+
+    class Boomers
+    {
+        public string GetCartoonReaction(bool isDark)
+        {
+            if(isDark)
+                return "Offended";
+            return "Laughing";
+        }
+    }
+
+    interface IPerson
+    {
+        string GetMemeReaction(bool isDark);
+    }
+
+    class BoomersAdapter : IPerson
+    {
+        private readonly Boomers boomers;
+
+        public BoomersAdapter(Boomers boomers)
+        {
+            this.boomers = boomers;
+        }
+        
+        public string GetMemeReaction(bool isDark)
+        {
+            return boomers.GetCartoonReaction(isDark);
+        }
+    }
+
+    class Millennials : IPerson
+    {
+        public string GetMemeReaction(bool isDark)
+        {
+            return "Laughing";
+        }
     }
 }
