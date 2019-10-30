cda2e23bb2ff69b89dc5609f1469149c22c0e12e
kishor4kumar@yahoo.com <kishor4kumar@yahoo.com>
2019-10-30 17:55:14 +0530

    Factory Pattern

--- DesignPatterns/Creational/Factory.cs
+++ DesignPatterns/Creational/Factory.cs
@@ -1,6 +1,62 @@
-﻿namespace DesignPatterns
+﻿using System;
+
+namespace DesignPatterns
 {
     class Factory
     {
+        public Factory()
+        {
+            var factory = new UIFactory();
+            var clientApplication = new ClientApplication(factory);
+            clientApplication.DrawUI();
+            clientApplication.ClickUI();
+        }
+    }
+
+    interface IButton
+    {
+        void Click();
+        void Render();
+    }
+
+    class CustomButton : IButton
+    {
+        public void Click()
+        {
+            Console.WriteLine("Custom button Click");
+        }
+
+        public void Render()
+        {
+            Console.WriteLine("Custom button Render");
+        }
+    }
+
+    class UIFactory
+    {
+        public IButton GetUIButton()
+        {
+            return new CustomButton();
+        }
+    }
+
+    class ClientApplication
+    {
+        private readonly IButton button;
+
+        public ClientApplication(UIFactory factory)
+        {
+            button = factory.GetUIButton();
+        }
+
+        public void DrawUI()
+        {
+            button.Render();
+        }
+
+        public void ClickUI()
+        {
+            button.Click();
+        }
     }
 }
